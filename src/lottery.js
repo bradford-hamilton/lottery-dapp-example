1f1b2b21dc0ac4596c3daf1dc2509e4ec0e9d74c
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-02-09 21:21:44 -0700

    [feature] pull in abi and contract deploy address from lottery contract

---
+++ src/lottery.js
@@ -0,0 +1,58 @@
+import web3 from './web3';
+
+const address = '0xB5789e3E6A900baF95E50fA067C62Bc4bBeeDbE7';
+const abi = [
+  {
+    constant: false,
+    inputs: [],
+    name: 'selectWinner',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'manager',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'enterLottery',
+    outputs: [],
+    payable: true,
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'getAllPlayers',
+    outputs: [{ name: '', type: 'address[]' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [{ name: '', type: 'uint256' }],
+    name: 'players',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+];
+
+export default new web3.eth.Contract(abi, address);
